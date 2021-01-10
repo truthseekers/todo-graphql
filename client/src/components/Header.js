7041995ade85feabb49ea32b8275642b2a16c876
John Curry <fastpenguin91@gmail.com>
2021-01-10 12:08:44 -0800

    ui routing done. need link style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,24 +2,28 @@ import React, { Fragment } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import MenuItem from "@material-ui/core/MenuItem";
+import { useHistory, Link } from "react-router-dom";
 
 function Header() {
-  //   const currentUser = { firstName: "Bobby" };
-  const currentUser = false;
+  let history = useHistory();
+  const currentUser = { firstName: "Bobby" };
+  // const currentUser = false;
 
   return (
     <div>
       <AppBar position="sticky">
         <Toolbar>
-          <MenuItem>Home</MenuItem>
+          <MenuItem onClick={() => history.push("/")}>Home</MenuItem>
           {currentUser ? (
             <Fragment>
               <MenuItem>Logout</MenuItem>
-              <MenuItem>Dashboard</MenuItem>
+              <MenuItem>
+                <Link to="/dashboard">Dashboard</Link>
+              </MenuItem>
               <span>Welcome {currentUser.firstName}</span>
             </Fragment>
           ) : (
-            <MenuItem>Login</MenuItem>
+            <MenuItem onClick={() => history.push("/login")}>Login</MenuItem>
           )}
         </Toolbar>
       </AppBar>
