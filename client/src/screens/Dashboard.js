997ec7e0e77b1ca6e48b34c233054a490bdaa057
John Curry <fastpenguin91@gmail.com>
2021-01-11 18:36:09 -0800

    something is wrong

--- client/src/screens/Dashboard.js
+++ client/src/screens/Dashboard.js
@@ -1,4 +1,7 @@
-import { useQuery, gql } from "@apollo/client";
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { useQuery, useMutation, gql } from "@apollo/client";
 import Todos from "../components/Todos";
 
 const TODOS_QUERY = gql`
@@ -11,17 +14,55 @@ const TODOS_QUERY = gql`
   }
 `;
 
+const NEW_TODO = gql`
+  mutation createTodo($newTodo: String!, $userId: ID!, $isComplete: Boolean!) {
+    createTodo(name: $newTodo, isComplete: $isComplete, userId: $userId) {
+      id
+      name
+      isComplete
+      userId
+    }
+  }
+`;
+
 function Dashboard() {
   const { data, loading, error } = useQuery(TODOS_QUERY, {});
+  const [dashInput, setDashInput] = useState("");
+  const [createTodo] = useMutation(NEW_TODO);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createTodo({
+      variables: {
+        newTodo: dashInput,
+        userId: 1,
+        isComplete: false,
+      },
+    });
+    setDashInput("");
+  };
+
   console.log("data", data);
 
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="dashInput"
+          label="add a todo"
+          value={dashInput}
+          onChange={(e) => setDashInput(e.target.value)}
+          variant="outlined"
+          margin="normal"
+        />
+      </form>
+      <Button type="submit" variant="contained" color="primary">
+        Add Todo
+      </Button>
       <Todos todoItems={data.todos} />
     </div>
   );
